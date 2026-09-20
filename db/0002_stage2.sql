c931b7015d4748f866f9da18a4a243009cbf8201
muradmustafazad80-eng <muradmustafazad80@gmail.com>
2026-09-20 15:04:39 +0400

    kk

--- db/0002_stage2.sql
+++ db/0002_stage2.sql
@@ -95,7 +95,6 @@ DO $$ BEGIN
   END IF;
 END $$;
 
-DROP INDEX IF EXISTS "Service_name_key";
 DROP INDEX IF EXISTS "Barber_name_key";
 DROP INDEX IF EXISTS "BusinessSetting_key_key";
 CREATE UNIQUE INDEX IF NOT EXISTS "Customer_businessPhone_key" ON "Customer" ("businessId", "phone");
